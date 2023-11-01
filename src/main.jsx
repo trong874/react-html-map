da4f68fa9489bd36dcfefd31a63f45893ea73112
Phạm Văn Trọng <pvt874@gmail.com>
2023-11-02 01:38:04 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-html-map/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
