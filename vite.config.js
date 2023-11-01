d12f3d4ae44cb7290a7c872e8eca96163df70fe1
Phạm Văn Trọng <pvt874@gmail.com>
2023-11-02 01:44:59 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { log } from 'console'
 // https://vitejs.dev/config/
-export default defineConfig((command) => {
+export default defineConfig(({command}) => {
   const config = {
     plugins: [react()],
     resolve: {
