936f3c43675010a8d5994daaeb4886efe78855fe
Phạm Văn Trọng <pvt874@gmail.com>
2023-11-02 01:34:25 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      components: `${path.resolve(__dirname, "./src/components/")}`,
-      public: `${path.resolve(__dirname, "./public/")}`,
-      pages: path.resolve(__dirname, "./src/pages"),
-      types: `${path.resolve(__dirname, "./src/@types")}`,
-      assets: `${path.resolve(__dirname, "./src/assets")}`,
+export default defineConfig(() => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+        components: `${path.resolve(__dirname, "./src/components/")}`,
+        public: `${path.resolve(__dirname, "./public/")}`,
+        pages: path.resolve(__dirname, "./src/pages"),
+        types: `${path.resolve(__dirname, "./src/@types")}`,
+        assets: `${path.resolve(__dirname, "./src/assets")}`,
+      },
     },
-  },
-  base: '/react-html-map/'
+  }
+  if (command !== 'serve') {
+    config.base = '/react-html-map/'
+  }
+  return config;
 })
